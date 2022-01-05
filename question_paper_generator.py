ae24c559c9520f291e7ec249ceced9bdf50ee191
sumedhdkme19 <72097989+sumedhdkme19@users.noreply.github.com>
2022-01-05 21:37:06 +0530

    Update commit-1

--- question_paper_generator.py
+++ question_paper_generator.py
@@ -1,18 +1,51 @@
-def read_csv_file(path):
+import pandas as pd
+import os
+import random
+import shutil
+
+from pandas.io.parsers import read_csv
+
+if(os.path.isdir("QP_Repository")):
     pass
+else:
+    os.mkdir("QP_Repository")
 
-def write_questions(l1, l2, l3):
-    try:
-        if(len(l1) !=0 and len(l2) !=0 and len(l3)!=0):
-            return "Success!"
-    except:
-        return "Failed"
+def read_excel_file(path, sheetname, n, m):
+    lst = []
+    df = pd.read_excel(path, sheet_name = sheetname)
+    for j in range(m):
+        lst.append([a for a in [df.iloc[i,j] for i in range(n)]])
+    return lst
+    
 
-if __name__ == '__main__':
-    path = ''
-    l1, l2, l3 = read_csv_file(path)
-    result = write_questions(l1, l2, l3)
-    print(result)
+def write_into(string, i):
+    file_name = f'SOM_CIE_{i}'
+    file_path = ''
 
+    with open(file_name, 'a') as f:
+        f.write('\n\n')
+        f.write('Q1. '+string)
+    
+
+
+if __name__ == '__main__' :
+        path = 'ATT_Sample_Question.xlsx'
+        lst = read_excel_file(path, "Sheet2", 4, 5)
+        c = 1
+        p1, p2, p3, p4, p5, p6 = lst
+        for i in p1:
+            for j in p2:
+                for k in p3:
+                    for l in p4:
+                        for o in p5:
+                            for p in p6:
+                                string = ' '.join([i, j, k, l, o, p])
+                                write_into(string, c)
+                                c += 1
+
+        print("SUCCESS!")
+
+    
+    
     
 
